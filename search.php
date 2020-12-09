ca3a5f776f3a6e5b66daa4c9b5e5ac6dd5334b33
Rbn DEV <62131778+rbnndev@users.noreply.github.com>
2020-12-08 23:37:28 -0300

    UPDATE

--- search.php
+++ search.php
@@ -4,30 +4,35 @@ include("conexao.php");
 $idpedido = $_POST['id'];
 
 echo '<div id="resultpesquisa">';
+if(is_numeric($idpedido)){
+    $get = mysqli_query($conn, "SELECT * FROM `pedidos` WHERE id = $idpedido");
+    if(mysqli_num_rows($get) > 0){
+        while($rows = mysqli_fetch_array($get)){
+            $id = $rows["id"];
+            $nome = $rows["cliente"];
+            echo "<h3 id='title-search'>#".$id . " ". $nome."</h3>";
+            }
 
-$get = mysqli_query($conn, "SELECT * FROM `pedidos` WHERE id = $idpedido");
-if(mysqli_num_rows($get) > 0){
-while($rows = mysqli_fetch_array($get)){
-    $id = $rows["id"];
-    $nome = $rows["cliente"];
-    echo "<h3 id='title-search'>#".$id . " ". $nome."</h3>";
-    }
 
-
-    $getitens = mysqli_query($conn, "SELECT * FROM `itens` WHERE pedido = $id ");
-    if(mysqli_num_rows($getitens) > 0){
-        while($itens = mysqli_fetch_array($getitens)){
-            $qto = $itens['qto'];
-            $produto = $itens['produto'];
-        
-            echo "<div id='search-itens'>";
-            echo $qto;
-            echo $produto."<br>";
-            echo "</div>";
-        }
+            $getitens = mysqli_query($conn, "SELECT * FROM `itens` WHERE pedido = $id ");
+            if(mysqli_num_rows($getitens) > 0){
+                while($itens = mysqli_fetch_array($getitens)){
+                    $qto = $itens['qto'];
+                    $produto = $itens['produto'];
+                
+                    echo "<div id='search-itens'>";
+                    echo $qto;
+                    echo $produto."<br>";
+                    echo "</div>";
+                }
+            }
+        }else{
+            echo "<p align='center'>Pedido não encontrado.</p>";
     }
 }else{
-    echo "Pedido não entrado.";
+    echo"<p align='center'>Os pedidos são apenas números.</p>";
 }
+
+
 echo '</div';
 ?>
