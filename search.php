af8692b8df431dd18bf88b13111a262f80d48daf
Rbn DEV <62131778+rbnndev@users.noreply.github.com>
2020-12-08 23:20:57 -0300

    VALIDAÇÃO

--- search.php
+++ search.php
@@ -6,21 +6,28 @@ $idpedido = $_POST['id'];
 echo '<div id="resultpesquisa">';
 
 $get = mysqli_query($conn, "SELECT * FROM `pedidos` WHERE id = $idpedido");
+if(mysqli_num_rows($get) > 0){
 while($rows = mysqli_fetch_array($get)){
     $id = $rows["id"];
     $nome = $rows["cliente"];
     echo "<h3 id='title-search'>#".$id . " ". $nome."</h3>";
-}
+    }
 
-$getitens = mysqli_query($conn, "SELECT * FROM `itens` WHERE pedido = $id ");
-while($itens = mysqli_fetch_array($getitens)){
-    $qto = $itens['qto'];
-    $produto = $itens['produto'];
 
-    echo "<div id='search-itens'>";
-    echo $qto;
-    echo $produto."<br>";
-    echo "</div>";
+    $getitens = mysqli_query($conn, "SELECT * FROM `itens` WHERE pedido = $id ");
+    if(mysqli_num_rows($getitens) > 0){
+        while($itens = mysqli_fetch_array($getitens)){
+            $qto = $itens['qto'];
+            $produto = $itens['produto'];
+        
+            echo "<div id='search-itens'>";
+            echo $qto;
+            echo $produto."<br>";
+            echo "</div>";
+        }
+    }
+}else{
+    echo "Pedido não entrado.";
 }
 echo '</div';
 ?>
